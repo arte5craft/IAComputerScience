46914e4a47f6c24e84e8db54820a21a24c451c71
S Anand <root.node@gmail.com>
2023-10-21 12:39:36 +0800

    STY: Reformat with prettier. Fix eslint issues

--- js/calendar.js
+++ js/calendar.js
@@ -1,14 +1,14 @@
 // Sample input data
-const subjects = ['A', 'B', 'C'];
-const priorityScores = {'A': 3, 'B': 2, 'C': 1};
-const previousWeekHours = {'A': 5, 'B': 3, 'C': 2};
-const breaksTaken = {'A': 2, 'B': 1, 'C': 1};
-const totalHours = 15;  // Total hours to be divided this week
+const subjects = ["A", "B", "C"];
+const priorityScores = { A: 3, B: 2, C: 1 };
+const previousWeekHours = { A: 5, B: 3, C: 2 };
+const breaksTaken = { A: 2, B: 1, C: 1 };
+const totalHours = 15; // Total hours to be divided this week
 
 // Calculate weighted priority scores
 const weightedPriorityScores = {};
-subjects.forEach(subject => {
-    weightedPriorityScores[subject] = priorityScores[subject] * (previousWeekHours[subject] - breaksTaken[subject] + 1);
+subjects.forEach((subject) => {
+  weightedPriorityScores[subject] = priorityScores[subject] * (previousWeekHours[subject] - breaksTaken[subject] + 1);
 });
 
 // Sort subjects by weighted priority scores in descending order
@@ -19,12 +19,12 @@ const allocatedHours = {};
 let totalAllocatedHours = 0;
 
 // Allocate hours based on priority scores and previous week data
-sortedSubjects.forEach(subject => {
-    const maxHoursToAllocate = previousWeekHours[subject] - breaksTaken[subject] + 1;
-    const remainingHours = totalHours - totalAllocatedHours;
-    const hoursToAllocate = Math.min(remainingHours, maxHoursToAllocate);
-    allocatedHours[subject] = hoursToAllocate;
-    totalAllocatedHours += hoursToAllocate;
+sortedSubjects.forEach((subject) => {
+  const maxHoursToAllocate = previousWeekHours[subject] - breaksTaken[subject] + 1;
+  const remainingHours = totalHours - totalAllocatedHours;
+  const hoursToAllocate = Math.min(remainingHours, maxHoursToAllocate);
+  allocatedHours[subject] = hoursToAllocate;
+  totalAllocatedHours += hoursToAllocate;
 });
 
 console.log("Allocated Hours:", allocatedHours);
