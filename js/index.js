798074a6c278ad847d1a09fcfaa5a0cabfafb4e5
S Anand <root.node@gmail.com>
2023-10-21 12:39:36 +0800

    STY: Reformat with prettier. Fix eslint issues

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
+/* globals bootstrap, db, uid */
+
 let countdown;
 let timerRunning = false;
 let timeLeft = 0;
 var formDataObject;
 let pauseNum = 0;
-let startTime;
-let endTime;
 
-const form = document.querySelector('.studyForm');
-const timerDisplay = document.getElementById('timer');
-const startButton = document.getElementById('submitBtn');
-const pauseButton = document.getElementById('pause');
-const resumeButton = document.getElementById('resume');
-const clearButton = document.getElementById('clear');
+const form = document.querySelector(".studyForm");
+const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("submitBtn");
+const pauseButton = document.getElementById("pause");
+const resumeButton = document.getElementById("resume");
+const clearButton = document.getElementById("clear");
 
-const modTime = document.getElementById('timeSpent');
-const modPoints = document.getElementById('points');
-const modPauses = document.getElementById('numPause');
-const modSubject = document.getElementById('subject');
-const modPriority = document.getElementById('priority');
-const modGoals = document.getElementById('goals');
+const modTime = document.getElementById("timeSpent");
+const modPoints = document.getElementById("points");
+const modPauses = document.getElementById("numPause");
+const modSubject = document.getElementById("subject");
+const modPriority = document.getElementById("priority");
+const modGoals = document.getElementById("goals");
 
 function displayTimeLeft(seconds) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainderSeconds.toString().padStart(2, '0')}`;
+  const display = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${remainderSeconds
+    .toString()
+    .padStart(2, "0")}`;
   timerDisplay.textContent = display;
 }
 
@@ -46,9 +48,9 @@ function startTimer(seconds) {
   clearButton.disabled = false;
   countdown = setInterval(() => {
     timeLeft--;
-    localStorage.setItem('time', timeLeft);
+    localStorage.setItem("time", timeLeft);
     if (timeLeft < 0) {
-      let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+      let myModal = new bootstrap.Modal(document.getElementById("exampleModal"), {});
       myModal.show();
       clearInterval(countdown);
       saveData(formDataObject);
@@ -65,33 +67,33 @@ function startTimer(seconds) {
 
 function saveData(data) {
   db.collection("users")
-  .doc(uid)
-  .collection("schedules")
-  .doc()
-  .set(data)
-  .then(() => {
-    console.log("success");
-  })
-  .catch((error) => {
-    console.error("Error adding document: ", error);
-  });
+    .doc(uid)
+    .collection("schedules")
+    .doc()
+    .set(data)
+    .then(() => {
+      console.log("success");
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
 }
 
-startButton.addEventListener('click', () => {
-  var hours = document.getElementById('hours').value;
-  var minutes = document.getElementById('minutes').value;
+startButton.addEventListener("click", () => {
+  var hours = document.getElementById("hours").value;
+  var minutes = document.getElementById("minutes").value;
   var formData = new FormData(form);
   formDataObject = Object.fromEntries(formData);
-  modTime.innerHTML = (hours).toString().padStart(2, '0') + ':' + (minutes).toString().padStart(2, '0') + ":00";
-  modPoints.innerHTML = (hours*10 + minutes) + ' points';
-  modSubject.innerHTML = formData.get('subject');
-  modPriority.innerHTML = formData.get('priority');
-  modGoals.innerHTML = formData.get('goals');
-  startTimer(hours*3600 + minutes*60);
+  modTime.innerHTML = hours.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0") + ":00";
+  modPoints.innerHTML = hours * 10 + minutes + " points";
+  modSubject.innerHTML = formData.get("subject");
+  modPriority.innerHTML = formData.get("priority");
+  modGoals.innerHTML = formData.get("goals");
+  startTimer(hours * 3600 + minutes * 60);
   form.reset();
 });
 
-pauseButton.addEventListener('click', () => {
+pauseButton.addEventListener("click", () => {
   clearInterval(countdown);
   timerRunning = false;
   pauseNum++;
@@ -99,11 +101,11 @@ pauseButton.addEventListener('click', () => {
   resumeButton.disabled = false;
 });
 
-resumeButton.addEventListener('click', () => {
+resumeButton.addEventListener("click", () => {
   startTimer(timeLeft);
 });
 
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener("click", () => {
   clearInterval(countdown);
   timerRunning = false;
   startButton.disabled = false;
@@ -112,7 +114,3 @@ clearButton.addEventListener('click', () => {
   clearButton.disabled = true;
   displayTimeLeft(0);
 });
-
-function points() {
-  // create a doc to store points for each subject if it does not exist
-}
